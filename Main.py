9486afe9178e228aae33438c9108114d1cc468cc
vkajtih <43199411+vkajtih@users.noreply.github.com>
2020-01-26 19:21:20 +0530

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,105 @@
+import keras
+#DataGenerator
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, df, batch_size = 16, subset="train", shuffle=False, 
+                 preprocess=None, info={}):
+        super().__init__()
+        self.df = df
+        self.shuffle = shuffle
+        self.subset = subset
+        self.batch_size = batch_size
+        self.preprocess = preprocess
+        self.info = info
+        
+        if self.subset == "train":
+            self.data_path = path + 'train_images/'
+        elif self.subset == "test":
+            self.data_path = path + 'test_images/'
+        self.on_epoch_end()
+
+    def __len__(self):
+        return int(np.floor(len(self.df) / self.batch_size))
+    
+    def on_epoch_end(self):
+        self.indexes = np.arange(len(self.df))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+    
+    def __getitem__(self, index): 
+        X = np.empty((self.batch_size,128,800,3),dtype=np.float32)
+        y = np.empty((self.batch_size,128,800,4),dtype=np.int8)
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+        for i,f in enumerate(self.df['ImageId'].iloc[indexes]):
+            self.info[index*self.batch_size+i]=f
+            X[i,] = Image.open(self.data_path + f).resize((800,128))
+            if self.subset == 'train': 
+                for j in range(4):
+                    y[i,:,:,j] = rle2maskResize(self.df['e'+str(j+1)].iloc[indexes[i]])
+        if self.preprocess!=None: X = self.preprocess(X)
+        if self.subset == 'train': return X, y
+        else: return X
+#Utility Function definition
+
+def rle2maskResize(rle):
+    # CONVERT RLE TO MASK 
+    if (pd.isnull(rle))|(rle==''): 
+        return np.zeros((128,800) ,dtype=np.uint8)
+    
+    height= 256
+    width = 1600
+    mask= np.zeros( width*height ,dtype=np.uint8)
+
+    array = np.asarray([int(x) for x in rle.split()])
+    starts = array[0::2]-1
+    lengths = array[1::2]    
+    for index, start in enumerate(starts):
+        mask[int(start):int(start+lengths[index])] = 1
+    
+    return mask.reshape( (height,width), order='F' )[::2,::2]
+
+def mask2contour(mask, width=3):
+    # CONVERT MASK TO ITS CONTOUR
+    w = mask.shape[1]
+    h = mask.shape[0]
+    mask2 = np.concatenate([mask[:,width:],np.zeros((h,width))],axis=1)
+    mask2 = np.logical_xor(mask,mask2)
+    mask3 = np.concatenate([mask[width:,:],np.zeros((width,w))],axis=0)
+    mask3 = np.logical_xor(mask,mask3)
+    return np.logical_or(mask2,mask3) 
+
+def mask2pad(mask, pad=2):
+    # ENLARGE MASK TO INCLUDE MORE SPACE AROUND DEFECT
+    w = mask.shape[1]
+    h = mask.shape[0]
+    
+    # MASK UP
+    for k in range(1,pad,2):
+        temp = np.concatenate([mask[k:,:],np.zeros((k,w))],axis=0)
+        mask = np.logical_or(mask,temp)
+    # MASK DOWN
+    for k in range(1,pad,2):
+        temp = np.concatenate([np.zeros((k,w)),mask[:-k,:]],axis=0)
+        mask = np.logical_or(mask,temp)
+    # MASK LEFT
+    for k in range(1,pad,2):
+        temp = np.concatenate([mask[:,k:],np.zeros((h,k))],axis=1)
+        mask = np.logical_or(mask,temp)
+    # MASK RIGHT
+    for k in range(1,pad,2):
+        temp = np.concatenate([np.zeros((h,k)),mask[:,:-k]],axis=1)
+        mask = np.logical_or(mask,temp)
+    
+    return mask 
+
+#Train EDA
+
+plt.figure(figsize=(13.5,2.5))
+bar = plt.bar( [1,2,3,4],100*np.mean( train2.iloc[:,1:5]!='',axis=0) )
+plt.title('Percent Training Images with Defect', fontsize=16)
+plt.ylabel('Percent of Images'); plt.xlabel('Defect Type')
+plt.xticks([1,2,3,4])
+for rect in bar:
+    height = rect.get_height()
+    plt.text(rect.get_x() + rect.get_width()/2.0, height, '%.1f %%' % height,
+             ha='center', va='bottom',fontsize=16)
+plt.ylim((0,50)); plt.show()
